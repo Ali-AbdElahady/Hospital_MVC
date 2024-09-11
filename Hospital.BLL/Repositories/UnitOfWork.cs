3e7beef773031de6537b91d57e6779fb09c5d369
Ali-AbdElahady <aliahmed952021@gmail.com>
2024-09-11 17:06:19 +0300

    null exp

--- Hospital.BLL/Repositories/UnitOfWork.cs
+++ Hospital.BLL/Repositories/UnitOfWork.cs
@@ -1,6 +1,8 @@
 ﻿using Hospital.BLL.Interfaces;
 using Hospital.DAL.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +10,30 @@ using System.Threading.Tasks;
 
 namespace Hospital.BLL.Repositories
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
         private readonly HospitalDbContext _dbContext;
+        private Hashtable _repositorys;
 
         public UnitOfWork(HospitalDbContext dbContext)
         {
             this._dbContext = dbContext;
+			_repositorys = new Hashtable();
         }
+
+        public async Task<int> CompleteAsync() { return await _dbContext.SaveChangesAsync(); }
+
+        public async ValueTask DisposeAsync() => await _dbContext.DisposeAsync();
+
         public IGenericRepository<T> GenerateGenericRepo<T>() where T : class
         {
-            IGenericRepository<T> repo = new GenericRepository<T>(_dbContext);
-            return repo;
+            var type = typeof(T).Name; 
+            if (!_repositorys.ContainsKey(type))
+            {
+                var Repository = new GenericRepository<T>(_dbContext);
+                _repositorys.Add(type, Repository);
+            }
+            return _repositorys[type] as IGenericRepository<T>;
         }
     }
 }
--- Hospital.PL/Areas/Admin/Controllers/HospitalsController.cs
+++ Hospital.PL/Areas/Admin/Controllers/HospitalsController.cs
@@ -6,21 +6,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Hospital.PL.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("Admin/Hospitals")]
     public class HospitalsController : Controller
     {
         private readonly BLL.Interfaces.IUnitOfWork unitOfWork;
-        private readonly ILogger logger;
+        private readonly ILogger<HospitalsController> logger;
 
-        public HospitalsController(IUnitOfWork unitOfWork,ILogger logger)
+        public HospitalsController(IUnitOfWork unitOfWork,ILogger<HospitalsController> logger)
         {
             this.unitOfWork = unitOfWork;
             this.logger = logger;
         }
+        [Route("")]
+        [Route("Index")]
         public async Task<IActionResult> Index(EntitySpecParams parms)
         {
             var spec = new HospitalsSpecifications(parms);
             var hospitals = await unitOfWork.GenerateGenericRepo<HospitalEntity>().GetAllWithSpecAsync(spec);
-            logger.LogInformation($"hospitals {hospitals}");
+            logger.LogInformation($"Retrieved hospitals: {hospitals.Count()} items.");
             return View(hospitals);
         }
     }
