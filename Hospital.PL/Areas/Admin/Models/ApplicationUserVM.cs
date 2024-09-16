31221f26680d48dffcc278fbde285039b731e47a
Ali-AbdElahady <aliahmed952021@gmail.com>
2024-09-16 16:58:39 +0300

    fix id format in ApplicationUserVM

--- Hospital.PL/Areas/Admin/Models/ApplicationUserVM.cs
+++ Hospital.PL/Areas/Admin/Models/ApplicationUserVM.cs
@@ -4,11 +4,11 @@ namespace Hospital.PL.Areas.Admin.Models
 {
     public class ApplicationUserVM
     {
-        public int Id { get; set; }
+        public string Id { get; set; } // Change from int to string
         public string FName { get; set; }
         public string LName { get; set; }
         public string? Address { get; set; }
-        public int? Specialization_ID { get; set; } //  Foreign Key of Specialization for Doctor
+        public int? Specialization_ID { get; set; } // Foreign Key of Specialization for Doctor
         public Specialization Specialization { get; set; } // Navigational Property
         public int? Department_ID { get; set; } // Foreign Key of Department for Staff And Doctor
         public Department Department { get; set; } // Navigational Property
